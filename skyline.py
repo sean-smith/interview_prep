04822dcb738b5b7b814d565eb14892a25091a546
Sean Smith <swsmith@bu.edu>
2015-03-13 20:45:01 -0700

    doing the skyline problem

--- skyline.py
+++ skyline.py
@@ -18,18 +18,30 @@ list = [(line.split()[0], (line.split()[1], line.split()[2]))  for line in sys.s
 heapq.heapify(list)
 
 
+def skyline(length, building):
+	s,r = building
+	h,e = r
 
-def skyline(item):
-	s,h,e = item
 
-
-	#The start value	
-	item = heapq.pop(list)
-	if item.start == s and item.height > height:
-		print()
-
+	#The start value
+	s_1, r = heapq.pop(list)
+	h_1, e_1 = r	
+	if s == s_1 and h_1 > h:
+		length, building  = skyline(length, (s_1, r))
+	else:
+		heapq.push((s_1, r))
+		length += building[0]
 
 	#The top portion
+	s,r = building
+	h,e = r
+	s_1, r = heapq.pop(list)
+	h_1, e_1 = r	
+	if s_1 > e and h_1 > h:
+		length, building  = skyline(length+(s_1-s), (s_1, r))
+	else:
+		heapq.push((s_1, r))
+		length += building[0]
 
 
 	#The down portion
@@ -40,4 +52,4 @@ def skyline(item):
 
 
 
-#print(skyline(heapq.pop(list)))
+print(skyline(0, heapq.pop(list)))
