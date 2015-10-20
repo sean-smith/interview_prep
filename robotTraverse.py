c3b658708044d2ce780d88537738b224ddd5b31e
Sean Smith <swsmith@bu.edu>
2015-10-20 02:36:50 -0400

    As in Protestant Europe, by contrast, where sects divided endlessly into smaller competing sects and no church dominated any other, all is different in the fragmented world of IBM.  That realm is now a chaos of conflicting norms and standards that not even IBM can hope to control.  You can buy a computer that works like an IBM machine but contains nothing made or sold by IBM itself.  Renegades from IBM constantly set up rival firms and establish standards of their own.  When IBM recently abandoned some of its original standards and decreed new ones, many of its rivals declared a puritan allegiance to IBM's original faith, and denounced the company as a divisive innovator.  Still, the IBM world is united by its distrust of icons and imagery.  IBM's screens are designed for language, not pictures.  Graven images may be tolerated by the luxurious cults, but the true IBM faith relies on the austerity of the word. 		-- Edward Mendelson, "The New Republic", February 22, 1988

--- robotTraverse.py
+++ robotTraverse.py
@@ -27,33 +27,40 @@ def parseFile(path):
 
 	f.close()
 
-	return map, robot_x, robot_y
-
-	print "robot_x = "+str(robot_x)+" robot_y = "+str(robot_y)
+	return map, robot_x, robot_y, max_x, max_y
 
 
 
+map = [['.', '.', '.'],['R', 'O', 'T']]
+max_x = 3
+max_y = 2
+
+# Start at robot's location and recurse in 4 directions
+# Fill in a R in each unvisited square
 def traverse(x, y):
-	print "x, y = "+str(x)+","+str(y)+"\ndot = "+map[y][x]
+	# print "x, y = "+str(x)+","+str(y)+"\ndot = "+map[y][x]
 
 	if map[y][x] == 'T':
 		return True
-
-
-	map[y][x] = 'R'
-	if x > 0:
-		traverse(x-1,y)
-	if x < (max_x - 1):
-		traverse(x+1,y)
-	if y > 0:
-		traverse(x,y-1)
-	if y < (max_y - 1):
-		traverse(x,y+1)
-	else:
+	elif map[y][x] == 'O':
 		return False
+	else:
+		# map[y][x] = 'R'
+		# print map
+		path1 = False
+		path2 = False
+		path3 = False 
+		path4 = False 
+		if x > 0:
+			path1 = traverse(x-1,y)
+		if x < (max_x - 1):
+			path2 = traverse(x+1,y)
+		if y > 0:
+			path3 = traverse(x,y-1)
+		if y < (max_y - 1):
+			path4 = traverse(x,y+1)
+		return path1 or path2 or path3 or path4
 
-print traverse(robot_x, robot_y)
-print map
 
 
 def main():
@@ -61,7 +68,10 @@ def main():
 		print "Input a map file!\nrobotTraverse.py [path_to_file]"
 		sys.exit()
 
-	parseFile(sys.arv[1])
+	# map, robot_x, robot_y, max_x, max_y = parseFile(sys.argv[1])
+	# print traverse(map, max_x, max_y, robot_x, robot_y)
+	print traverse(0, 1)
+
 
 
 main()
