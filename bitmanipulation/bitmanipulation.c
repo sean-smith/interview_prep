c3cf387e7e08e62dc86714c506d14284665e54d9
Sean Smith <swsmith@bu.edu>
2015-10-21 19:13:23 -0400

    The second best policy is dishonesty.

--- bitmanipulation/bitmanipulation.c
+++ bitmanipulation/bitmanipulation.c
@@ -17,8 +17,10 @@ int f1(int x)
 		return 1;
 	}else {
 		return 2;
-	}
+	} 
 }
+
+// Now no conditionals
 
 // 10 ^ 11 = 01
 // 01 ^ 11 = 10
@@ -40,14 +42,12 @@ int f3(int x)
 
 
 // Work in progress
-// 10 - 01 = 01
-// 01 - 01 = 00
-// 10 + 01 = 11
-// 01 + 01 = 10
+// 11 - 01 = 10
+// 11 - 10 = 01
 
 int f4(int x)
 {
-	return (x - 1);
+	return (3 - x);
 }
 
 // Same as left/right shift
@@ -57,14 +57,31 @@ int f5(int x)
 	return ((x*2) & 3) ^ (x / 2);
 }
 
+int f6(int x)
+{
+	return (~x & 3);
+}
+
+int f7(int x)
+{
+	switch (x) {
+		case 1:
+			return 2;
+		default:
+			return 1;
+	}
+}
+
 
 int main(int argc, char** argv) {
 	printf("f0(1) = %d, f0(2) = %d\n", f0(1), f0(2));
 	printf("f1(1) = %d, f1(2) = %d\n", f1(1), f1(2));
 	printf("f2(1) = %d, f2(2) = %d\n", f2(1), f2(2));
 	printf("f3(1) = %d, f3(2) = %d\n", f3(1), f3(2));
-	//printf("f4(1) = %d, f4(2) = %d\n", f4(1), f4(2));
+	printf("f4(1) = %d, f4(2) = %d\n", f4(1), f4(2));
 	printf("f5(1) = %d, f5(2) = %d\n", f5(1), f5(2));
+	printf("f6(1) = %d, f6(2) = %d\n", f6(1), f6(2));
+	printf("f7(1) = %d, f7(2) = %d\n", f7(1), f7(2));
 
 	return 1;
 }
